b570e3c2cc0421ede7adbc9ae2f63b88e35b3cb5
Joe Guerra <guerra_joe@hotmail.com>
2018-11-18 14:18:56 -0800

    ..

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-
+	searchkick
 	has_attached_file :image, styles: { medium: "400x600#", thumbnail: "100x200#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
